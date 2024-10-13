4f01eb1898b602e835f95c602790967b77faf8c9
Mark Craig <craig.mark.a@gmail.com>
2024-10-13 03:20:02 +0000

    enable ctest

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -14,6 +14,8 @@
 #include "linked_list.h"
 #include "test_linked_list.h"
 
+#define TEST_GET_NUM_NODES_NODE_CNT 10
+
 #define TEST_MOVE_NODE_NUM_NODES    10
 #define TEST_MOVE_NODE_IDX_1        3
 #define TEST_MOVE_NODE_IDX_2        7
@@ -222,4 +224,75 @@ int test_free_nth_node_in_list(int num_nodes) {
     } else {
         return 0;
     }
+}
+
+int main() {
+    int retval = 0, test_cnt_pass = 0, test_cnt_fail = 0;    
+
+    printf("Testing: get_num_nodes_circular_list()\n");
+    retval = test_get_num_nodes_circular_list(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: get_num_nodes_between_nodes()\n");
+    retval = test_get_num_nodes_between_nodes(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: insert_circular_list_in_circular_list()\n");
+    retval = test_insert_circular_list_in_circular_list(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: move_node_on_circular_list()\n");
+    retval = test_move_node_on_circular_list();
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: generate_linked_list()\n");
+    retval = test_generate_linked_list(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: move_largest_node_to_end_of_list()\n");
+    retval = test_move_largest_node_to_end_of_list(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: get_idx_of_smallest_val()\n");
+    retval = test_get_idx_of_smallest_val(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: free_nodes_in_list()\n");
+    retval = test_free_nodes_in_list(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Testing: free_nth_node_in_list()\n");
+    retval = test_free_nth_node_in_list(TEST_GET_NUM_NODES_NODE_CNT);
+    if (retval)
+        test_cnt_fail++;
+    else
+        test_cnt_pass++;
+
+    printf("Total Tests: %d PASS: %d FAIL: %d\n", test_cnt_fail + test_cnt_pass, test_cnt_pass, test_cnt_fail);
+    
+    return retval;
 }
